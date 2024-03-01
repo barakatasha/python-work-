ba4889803b12c4acf59a111b90a7fe9ee92dc6f4
barakatasha <muriminatasha@gmail.com>
2024-03-01 12:42:31 +0300

    first commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+name = input("What is your name: ")
+age = input("How old are you: ")
+location = input("Where do you live: ")
+print(f"Hello {name}! Great to know you are {age} and located in {location}.")
